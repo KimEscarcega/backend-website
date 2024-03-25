2762ff1af4d5c378bdc502be2ff840e2670312ff
Kim <159943632+KimEscarcega@users.noreply.github.com>
2024-03-24 21:53:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 const express = require("express");//create objects express 
 const app = express();
+const session = require("express-session");
 const bodyParser = require('body-parser');
 const router = require("./routes/pages");
-const Controller = require("./Controllers/auth.js")
 
 
+
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
+
+
 app.use("/", router);
-app.use("/", Controller );
+
+
 
 app.set("view engine", "ejs");
 
 
 
 app.use(express.static('Public'));
+
+
 app.listen(8080, ()=>{
     console.log("Sever started on post 8080")
 })
