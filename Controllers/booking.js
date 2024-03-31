441b9dbcaf225d4df54cfcc450ed783029ab652c
Evelyne11 <147781373+Evelyne11@users.noreply.github.com>
2024-03-31 17:56:17 -0500

    Update booking.js

--- Controllers/booking.js
+++ Controllers/booking.js
@@ -53,8 +53,55 @@ exports.lot = (req,res) =>{
 
 exports.confim=(req,res) =>{
     
-
 }
+
+//code for the pricing 
+exports.confirmation = (req, res) => {
+
+    const { lotnumber, appdate, startTime, endTime } = req.session;
+    const uID = req.body.uID;
+ 
+    req.session.appdate= date;
+    req.session.startTime=startTime;
+    req.session.endTime=endTime;
+    req.session.lotnumnber= lot;
+    req.session.totalPrice= Price;
+    
+    
+    //calculate start and endtime as a string
+    const startDate = new Date(`${appdate}T${startTime}`);
+    const endDate = new Date(`${appdate}T${endTime}`);
+
+    // this will calculate in milisec
+    const durationMs = endDate - startDate;
+
+    // from milli to minutes
+    const durationMinutes = durationMs / (1000 * 60);
+
+    // price is 15 mins for 0.45 cents
+    const pricePer15Minutes = 0.45;
+    const price = (durationMinutes / 15) * pricePer15Minutes;
+
+    console.log("Received data for confirmation:");
+    console.log("Lot Num:", lotnumber);
+    console.log("App Date:", appdate);
+    console.log("Start Time:", startTime);
+    console.log("End Time:", endTime);
+    console.log("Duration (minutes):", durationMinutes);
+    console.log("Price:", price);
+
+    // Pass data to the template
+    res.render("confirmation", { 
+        bookingrequest: {
+            lotnumber: lotnumber,
+            appdate: appdate,
+            startTime: startTime,
+            endTime: endTime,
+            totalPrice: price.toFixed(2) 
+        }
+    });
+};
+
  
 
 
