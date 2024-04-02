3a924bbdd3ea66df5ad4dd78317ef6d5514722f3
Evelyne11 <147781373+Evelyne11@users.noreply.github.com>
2024-04-01 21:37:07 -0500

    Update booking.js

--- Controllers/booking.js
+++ Controllers/booking.js
@@ -57,51 +57,56 @@ exports.confim=(req,res) =>{
 
 //code for the pricing 
 exports.confirmation = (req, res) => {
-
-    const { lotnumber, appdate, startTime, endTime } = req.session;
-    const uID = req.body.uID;
+    const date = req.body.date;
+    const startTime = req.body.startTime;
+    const endTime = req.body.endTime;
+    // const uID = req.body.uID;
+    const lot = req.body.lot;
  
-    req.session.appdate= date;
-    req.session.startTime=startTime;
-    req.session.endTime=endTime;
-    req.session.lotnumnber= lot;
-    req.session.totalPrice= Price;
-    
+    req.session.lot = lot;
+    req.session.appdate = date;
+    req.session.startTime = startTime;
+    req.session.endTime = endTime;
+    req.session.totalPrice = totalPrice;
+
+    if (!date || !startTime || !endTime || !uID || !lot || totalPrice){
+        res.render("confirmation", { 
+            bookingrequest: {
+                lot: req.session.lot,
+                appdate: date,
+                startTime: startTime,
+                endTime: endTime,
+                totalPrice: parseFloat(price.toFixed(2))
+            }
+        });
+
+    }
+
     
     //calculate start and endtime as a string
-    const startDate = new Date(`${appdate}T${startTime}`);
-    const endDate = new Date(`${appdate}T${endTime}`);
+    const startDate = new Date(`${date}T${startTime}`);
+    const endDate = new Date(`${date}T${endTime}`);
 
-    // this will calculate in milisec
+    // this will calculate in milliseconds
     const durationMs = endDate - startDate;
 
-    // from milli to minutes
+    // from milliseconds to minutes
     const durationMinutes = durationMs / (1000 * 60);
 
-    // price is 15 mins for 0.45 cents
+    // price of 15 mins for 0.45 cents
     const pricePer15Minutes = 0.45;
     const price = (durationMinutes / 15) * pricePer15Minutes;
 
     console.log("Received data for confirmation:");
-    console.log("Lot Num:", lotnumber);
-    console.log("App Date:", appdate);
+    console.log("Lot Num:", lot);
+    console.log("App Date:", date);
     console.log("Start Time:", startTime);
     console.log("End Time:", endTime);
     console.log("Duration (minutes):", durationMinutes);
     console.log("Price:", price);
 
-    // Pass data to the template
-    res.render("confirmation", { 
-        bookingrequest: {
-            lotnumber: lotnumber,
-            appdate: appdate,
-            startTime: startTime,
-            endTime: endTime,
-            totalPrice: price.toFixed(2) 
-        }
-    });
+  
 };
-
  
 
 
